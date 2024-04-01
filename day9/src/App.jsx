d91a930c699e5c881e5c958b32fde91bad8fd611
parkhyunseo <dazzle1999@naver.com>
2024-04-01 16:01:51 +0900

    update emotion

--- day9/src/App.jsx
+++ day9/src/App.jsx
@@ -2,12 +2,31 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { css } from "@emotion/css";
 
 function App() {
+  const [color, setColor] = useState("red");
   return (
     <>
+      {/* js에서 스타일을 줄 수 있기 때문에 css in js이라고 함 */}
+      <input type="color" onChange={(e) => setColor(e.target.value)} />
       <div className="box"></div>
       <div className="box"></div>
+
+      <div
+        className={css`
+          width: 200px;
+          height: 200px;
+          background-color: ${color};
+          &:hover {
+            background-color: blue;
+            transition: all 1s;
+            transform: rotate(900deg);
+            fontsize
+            
+          }
+        `}
+      ></div>
     </>
   );
 }
